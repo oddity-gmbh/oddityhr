70f13c8421c55873c8a1fd9cd786c2ee7162f5df
ememgee <24703421+EmEmGee@users.noreply.github.com>
2019-09-27 13:24:59 +0200

    code coverage and linting added

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -8,13 +8,14 @@ from flask_sqlalchemy import SQLAlchemy
 # instantiate the db
 db = SQLAlchemy()
 
+
 def create_app(script_info=None):
 
     # instantiate the app
     app = Flask(__name__)
 
     # set config
-    app_settings = os.getenv('APP_SETTINGS')
+    app_settings = os.getenv("APP_SETTINGS")
     app.config.from_object(app_settings)
 
     # set up extensions
@@ -22,11 +23,12 @@ def create_app(script_info=None):
 
     # register blueprints
     from project.api.users import users_blueprint
+
     app.register_blueprint(users_blueprint)
 
     # shell context for flask cli
     @app.shell_context_processor
     def ctx():
-        return {'app': app, 'db': db}
+        return {"app": app, "db": db}
 
     return app
